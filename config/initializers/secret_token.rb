ace7c5df44796f26dc7f7c61eb92abd6035961ff
Kevin Altman and Lake Denman <pair@thinkrelevance.com>
2012-02-29 21:03:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frisky::Application.config.secret_token = 'c9b2d4350c2dbd00e9f373cf1526816dc3ffd797ce9658a79d156823c6c429cdd0c45439852da161c9c3026a2b9aa75cbd75aa6d1bcbd28a6d08f8ceaabbe894'
